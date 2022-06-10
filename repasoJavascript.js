f7fe55527772d2785e47899887d1434826994167
fancisco Jimenez <devfran08@gmail.com>
2022-06-10 19:34:01 -0300

    PrimerClase: Declaracion de Variables

---
+++ repasoJavascript.js
@@ -0,0 +1,18 @@
+let datos = [
+  { name: "fran" },
+  { name: "ana" },
+  { name: "pedro" },
+  { name: "Gisell" },
+];
+
+datos = "Francisco";
+console.log(datos);
+
+// LAS VARIABLES DECLARADAS CON LET PUEDEN VARIAR
+// EN TIPO Y EN VALOR!!!!!!
+
+// ------------------------
+const PI = 3.141592653589793;
+console.log(datos2);
+
+// LAS VARIABLES DECLARADAS CON CONST NO PUEDEN VARIAR NI EN TIPO NI EN VALOR!!!!!!
